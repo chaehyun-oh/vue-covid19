b410c733857a76544560253ccd9c0c17f9c1de12
chaehyun_oh <ohchaehh@gmail.com>
2022-07-07 21:55:41 +0900

    vue.js practice project covid19 cases v.1 update

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+	<BasicLayout
+		@menuSelect="
+			(item) => {
+				content = item;
+			}
+		"
+	>
+		<MainContent :content="content" />
+	</BasicLayout>
+</template>
+
+<script>
+import BasicLayout from "./components/layouts/BasicLayout";
+import MainContent from "./components/MainContent";
+
+export default {
+	name: "App",
+	components: { BasicLayout, MainContent },
+	data() {
+		return {
+			content: {
+				label: "대시보드",
+				index: "Dashboard",
+				path: "main",
+			},
+		};
+	},
+};
+</script>
+
+<style></style>
---
+++ src/components/MainContent.vue
@@ -0,0 +1,41 @@
+<template>
+	<h2 class="content-title">{{ content.label }}</h2>
+	<component :is="targetComponent"></component>
+</template>
+
+<script>
+import { defineAsyncComponent } from "vue";
+
+export default {
+	name: "MainContent",
+	props: {
+		content: {
+			type: Object,
+			default() {
+				return {
+					label: "Test Menu",
+					path: "Test",
+					index: "TestMenu",
+				};
+			},
+		},
+	},
+	data() {
+		return {
+			targetComponent: null,
+		};
+	},
+	watch: {
+		content: {
+			deep: true,
+			handler: function (newValue) {
+				this.targetComponent = defineAsyncComponent(() =>
+					import(`@/components/${newValue.path}/${newValue.index}`)
+				);
+			},
+		},
+	},
+};
+</script>
+
+<style></style>
